d096bf27b3e5e9669b084e49966dfb1ed701770f
DESKTOP-FEJUPGQ\feder <federome@live.it>
2023-02-26 16:56:37 +0100

    cambiato in scena 2 le due sfere che girano su un centro, aggiornato lo script rotating object in modo tale che implementi una rotazione esponenziale

--- Assets/Script/RotatingObject.cs
+++ Assets/Script/RotatingObject.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class RotatingObject : MonoBehaviour
 {
+    public bool esponenzialSpeed;
     public float rotatingSpeed = 30f;
+    public float maxRotatingSpeed = 30000;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,15 @@ public class RotatingObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (esponenzialSpeed)
+        {
+            rotatingSpeed = rotatingSpeed + (Time.deltaTime * rotatingSpeed) * 0.1f;
+
+            if (rotatingSpeed > maxRotatingSpeed)
+            {
+                rotatingSpeed = maxRotatingSpeed;
+            }
+        }
         transform.Rotate(0, rotatingSpeed*Time.deltaTime, 0);
     }
 }
